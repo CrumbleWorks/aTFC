d16990884ff75234258a8553537b31d191c795f0
Michael Stocker <mic.sto@hotmail.com>
2021-01-14 00:15:27 +0100

    now with proper 'transparency'

--- src/main/java/org/crumbleworks/forge/aTFC/subscribers/ClientRegisterEvents.java
+++ src/main/java/org/crumbleworks/forge/aTFC/subscribers/ClientRegisterEvents.java
@@ -9,11 +9,14 @@ import org.crumbleworks.forge.aTFC.blocks.Tintable;
 import org.crumbleworks.forge.aTFC.wiring.Dirt;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 /**
@@ -36,8 +39,16 @@ public final class ClientRegisterEvents {
     }
 
     @SubscribeEvent
+    public static void registerTranslucency(FMLClientSetupEvent event) {
+        // FIXME we need to get array of blocks dynamically, this is wrong
+        RenderTypeLookup.setRenderLayer(Dirt.DIRT_BLOCK.get(),
+                RenderType.getCutout());
+    }
+
+    @SubscribeEvent
     public static void registerColorer(ColorHandlerEvent.Block event) {
+        // FIXME we need to get array of blocks dynamically, this is wrong
         event.getBlockColors().register(new BiomeAndRockBasedBlockColor(),
-                new Block[] { Dirt.DIRT_BLOCK.get() }); //FIXME we need to get array of blocks dynamically, this is wrong
+                new Block[] { Dirt.DIRT_BLOCK.get() });
     }
 }
