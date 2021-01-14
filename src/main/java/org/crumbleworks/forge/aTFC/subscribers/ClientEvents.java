85e303f22ff9dbba6c0554e756d13123762f2d9e
Michael Stocker <mic.sto@hotmail.com>
2021-01-14 11:12:27 +0100

    Färbig klappt, aber zu wellem Priis?

--- src/main/java/org/crumbleworks/forge/aTFC/subscribers/ClientEvents.java
+++ src/main/java/org/crumbleworks/forge/aTFC/subscribers/ClientEvents.java
@@ -1,12 +1,11 @@
 package org.crumbleworks.forge.aTFC.subscribers;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.crumbleworks.forge.aTFC.Main;
+import org.crumbleworks.forge.aTFC.Util;
 import org.crumbleworks.forge.aTFC.blocks.BiomeAndRockBasedBlockColor;
 import org.crumbleworks.forge.aTFC.blocks.Tintable;
-import org.crumbleworks.forge.aTFC.wiring.Dirt;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
@@ -17,7 +16,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
 /**
  * TODO
@@ -27,28 +25,20 @@ import net.minecraftforge.registries.ForgeRegistries;
  */
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Main.MOD_ID,
         bus = Bus.MOD)
-public final class ClientRegisterEvents {
-
-    private static Block[] blocks;
-    static {
-        Set<Block> _b = ForgeRegistries.BLOCKS.getValues().stream()
-                .filter((b) -> {
-                    return b instanceof Tintable;
-                }).collect(Collectors.toSet());
-        blocks = _b.toArray(new Block[_b.size()]);
-    }
+public final class ClientEvents {
 
     @SubscribeEvent
     public static void registerTranslucency(FMLClientSetupEvent event) {
-        // FIXME we need to get array of blocks dynamically, this is wrong
-        RenderTypeLookup.setRenderLayer(Dirt.DIRT_BLOCK.get(),
-                RenderType.getCutout());
+        Set<Block> blocks = Util.getBlocks(Tintable.class);
+        for(Block block : blocks) {
+            RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+        }
     }
 
     @SubscribeEvent
     public static void registerColorer(ColorHandlerEvent.Block event) {
-        // FIXME we need to get array of blocks dynamically, this is wrong
+        Set<Block> blocks = Util.getBlocks(Tintable.class);
         event.getBlockColors().register(new BiomeAndRockBasedBlockColor(),
-                new Block[] { Dirt.DIRT_BLOCK.get() });
+                blocks.toArray(new Block[blocks.size()]));
     }
 }
