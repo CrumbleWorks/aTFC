b797168fc1a0ef0ae1b73583a723d49c6daed7f8
McDonnough <psbaechli@gmail.com>
2021-01-28 19:53:57 +0100

    generalised goal to work for all kinds of mobs

--- src/main/java/org/crumbleworks/forge/aTFC/content/entities/goals/MoveAwayFromPlayerGoal.java
+++ src/main/java/org/crumbleworks/forge/aTFC/content/entities/goals/MoveAwayFromPlayerGoal.java
@@ -1,55 +1,55 @@
 package org.crumbleworks.forge.aTFC.content.entities.goals;
 
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 
 /**
- * Goal which makes an animal move away from a player if they get too close.
+ * Goal which makes a mob move away from a player if they get too close.
  *
  * @author Patrick Bächli
  * @since CURRENT_VERSION
  */
 public class MoveAwayFromPlayerGoal extends Goal {
 
-    private AnimalEntity animal;
+    private MobEntity mob;
     private double minDistance;
     private double speed;
 
     /**
-     * @param animal
-     *            An animal entity
+     * @param mob
+     *            A mob entity
      * @param minDistance
      *            Minimum distance to player. If the actual distance is lower
-     *            than this value, the animal will start to move away from the
+     *            than this value, the mob will start to move away from the
      *            player.
      * @param speed
-     *            Speed with which {@code animal} moves away from the player
+     *            Speed with which {@code mob} moves away from the player
      */
-    public MoveAwayFromPlayerGoal(AnimalEntity animal, double minDistance,
+    public MoveAwayFromPlayerGoal(MobEntity mob, double minDistance,
             double speed) {
-        this.animal = animal;
+        this.mob = mob;
         this.minDistance = minDistance;
         this.speed = speed;
     }
 
     @Override
     public boolean shouldExecute() {
-        return animal.world.getClosestPlayer(animal, minDistance) != null;
+        return mob.world.getClosestPlayer(mob, minDistance) != null;
     }
 
     @Override
     public void tick() {
-        PlayerEntity closestPlayer = animal.world.getClosestPlayer(animal,
+        PlayerEntity closestPlayer = mob.world.getClosestPlayer(mob,
                 minDistance);
 
-        BlockPos animalPosition = animal.getPosition();
+        BlockPos mobPosition = mob.getPosition();
         BlockPos playerPosition = closestPlayer.getPosition();
 
-        BlockPos difference = animalPosition.subtract(playerPosition);
-        BlockPos newTarget = animalPosition.add(difference);
-        animal.getNavigator().tryMoveToXYZ(newTarget.getX(), newTarget.getY(),
+        BlockPos difference = mobPosition.subtract(playerPosition);
+        BlockPos newTarget = mobPosition.add(difference);
+        mob.getNavigator().tryMoveToXYZ(newTarget.getX(), newTarget.getY(),
                 newTarget.getZ(), speed);
     }
 
